ed267d5311d403903acabcb7b7c35eb6b2040647
Jan-Jaap <62014232+JanJaapvG@users.noreply.github.com>
2021-10-11 17:00:52 +0200

    Added Meteoroid impact

---
+++ POTATO/Assets/Scripts/GetMesh.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GetMesh : MonoBehaviour
+{
+    public Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Get the mesh of the Component
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Check if collider is a Meteoroid
+        if (collision.collider.CompareTag("Meteoroid"))
+        {
+            //Get all the vertices of the Component in an array
+            Vector3[] vertices = new Vector3[mesh.vertices.Length];
+
+            //Create a copy of all the vertices in the Component
+            System.Array.Copy(mesh.vertices, vertices, vertices.Length);
+
+            //Check all the contactpoints of the collision
+            foreach (ContactPoint c in collision.contacts)
+            {
+                Rigidbody meteoroid = collision.collider.GetComponent<Rigidbody>();
+
+                var speed = meteoroid.mass * meteoroid.velocity;
+                Debug.Log(meteoroid.mass);
+                Debug.Log(meteoroid.velocity);
+                Debug.Log(speed);
+
+                //For loop that checks all the copied vertices of the component
+                for (int i=0; i < vertices.Length; i++) {
+
+                   //Gets all the copied vertices within a certain distance of the contact point
+                   if (Vector3.Distance(transform.TransformPoint(vertices[i]), c.point) <= 10)
+                    {
+                        Debug.Log(vertices[i]);
+                        //Changes the position of the copied vertices
+                        vertices[i] = (vertices[i] + transform.InverseTransformVector(c.normal * 10));
+                        Debug.Log("Changed vertices" + vertices[i]);
+                    }
+                }
+            }
+
+            //Changes the vertices of the component to the changed vertices
+            mesh.vertices = vertices;
+
+            //Recalculate the position of the changed vertices
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+            mesh.RecalculateTangents();
+
+            //Destroy the Meteoroid
+            Destroy(collision.gameObject);
+        }
+            
+    }
+
+
+
+}
